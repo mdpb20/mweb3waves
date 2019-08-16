7a1f14230062b9694520b113e9149d2a76cdeff2
mdpb20 <mdpb20@gmail.com>
2019-08-16 23:35:02 +0200

    ;

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,3 @@
-
 import ReactDOM from "react-dom";
 import React from 'react';
 
@@ -6,12 +5,28 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {}
+        this.authFunc = this.authFunc.bind(this);
+    }
+    authFunc() {
+        const authData = { data: "Autorizar en mi sitio" };
+        if (WavesKeeper) {
+            WavesKeeper.auth( authData )
+            .then(auth => {
+                console.log( auth ); // muestra los resultados en la consola
+                /*...procesando la informacion */
+            }).catch(error => {
+                console.error( error ); // muestra los resultados en la consola
+                /*...procesando errores */
+            })
+        } else {
+            alert("Para autorizar, WavesKeeper debe estar instalado.");
+        }
     }
     render() {
         return (
             <div className="container">
-    		    <input className="btn btn-primary" type="submit" value="Alert" onClick={() => {alert("Alert button onClick");}}/>
-    	    </div>
+                <input className="btn btn-primary" type="submit" value="Auth" onClick={this.authFunc}/>
+            </div>
         )
     }
 }
